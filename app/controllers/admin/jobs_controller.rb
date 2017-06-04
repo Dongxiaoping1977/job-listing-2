c2277a81a63e43c14234eef3400cf3b9d0f681a7
Dongxiaoping1977 <13301412096@163.com>
2017-06-04 21:34:33 +0800

    change

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,7 +1,6 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
-
   def show
     @job = Job.find(params[:id])
   end
@@ -45,9 +44,16 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
+  def require_is_admin
+    if current_user.admin?
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
+  end
+
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
   end
 end
